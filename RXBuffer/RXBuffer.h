6a41da7388700acca98ee470f42ea61265684b5c
Rob Rix <rob.rix@me.com>
2013-01-16 14:36:45 -0500

    Initial Commit

---
+++ RXBuffer/RXBuffer.h
@@ -0,0 +1,13 @@
+//
+//  RXBuffer.h
+//  RXBuffer
+//
+//  Created by Rob Rix on 2013-01-16.
+//  Copyright (c) 2013 Rob Rix. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RXBuffer : NSObject
+
+@end
---
+++ RXBuffer/RXBuffer.m
@@ -0,0 +1,13 @@
+//
+//  RXBuffer.m
+//  RXBuffer
+//
+//  Created by Rob Rix on 2013-01-16.
+//  Copyright (c) 2013 Rob Rix. All rights reserved.
+//
+
+#import "RXBuffer.h"
+
+@implementation RXBuffer
+
+@end
